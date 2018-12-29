e46a4893d2d394e896fbcc1a434003e58d4a9026
valdot <45897344+valdot@users.noreply.github.com>
2018-12-29 18:19:01 +0800

    Create fastinternet.py

---
+++ fastinternet.py
@@ -0,0 +1,86 @@
+import os
+
+import time
+
+
+red = '\033[31;1m'
+green = '\033[32;1m'
+yellow = '\033[33;1m'
+blue = '\033[34;1m'
+purple = '\033[35;1m'
+cyan = '\033[36;1m'
+white = '\033[37;1m'
+
+time.sleep(1)
+
+os.system('clear')
+
+os.system("figlet MR.TVM4 |lolcat")
+print
+print "\033[36;1m[+]\033[32;1m============\033[36;1m Welcome My Tools \033[32;1m============\033[36;1m[+]"
+time.sleep(0.25)
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.25)
+print "\033[31;1m| ]\033[34;1m Author      \033[31;1m| \033[33;1MR.TVM4                       \033[31;1m |"
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.25)
+print "\033[31;1m| ]\033[34;1m Phone       \033[31;1m| \033[33;1m0895395533165                \033[31;1m |"
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.25)
+print "\033[31;1m| ]\033[34;1m Team Code   \033[31;1m| \033[33;1mValdo Taroreh             \033[31;1m |"
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.25)
+print "\033[31;1m| ]\033[34;1m Team Cyber  \033[31;1m| \033[33;1mHACK AJ4                 \033[31;1m |"
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.25)
+print "\033[31;1m| ]\033[34;1m             \033[31;1m| \033[33;1mBoyolali Cyber Security     \033[31;1m |"
+print "\033[35;1m[ ]==========================================[ ]"
+time.sleep(0.5)
+print
+print "\033[36;1m[x]\033[32;1m TOOLS MEMPERCEPAT JARINGAN INTERNET \033[36;1m[x]"
+time.sleep(0.25)
+print "\033[34;1m[ ]=====================================[ ]"
+time.sleep(0.25)
+print "\033[32;1m[1]\033[33;1m Metode Pertama                        \033[32;1m|"
+time.sleep(0.25)
+print "\033[34;1m[ ]=====================================[ ]"
+print "\033[32;1m[2]\033[33;1m Metode Kedua                          \033[32;1m|"
+time.sleep(0.25)
+print "\033[36;1m[x]\033[32;1m=====================================\033[36;1m[x]"
+print
+os.system("date |lolcat")
+print""
+micro = input("\033[36;1m root@V4LD0~$ \033[32;1m")
+
+
+if micro == 1:
+
+	os.system('clear')
+
+	os.system("figlet MR.TVM4 |lolcat")
+
+	time.sleep(0.5)
+
+	os.system("echo Jaringan Dipercepat.. |lolcat")
+
+	os.system("echo Done And Enjoy.. |lolcat")
+	
+	os.system("echo")
+	
+	os.system("ping 8.8.8.8")
+
+if micro == 2:
+
+	os.system('clear')
+
+	os.system("figlet MR.TVM4 |lolcat")
+
+	time.sleep(0.5)
+
+	os.system("echo Jaringan Dipercepat.. |lolcat")
+
+	os.system("echo Done And Enjoy.. |lolcat")
+	
+	os.system("echo")
+	
+	os.system("ping -D www.google.com")
